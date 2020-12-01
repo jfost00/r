1f9611d6aa55ae6155b6cbe9af1ff3d0d067aeff
jfost00 <jeffreybfoster@yahoo.com>
2020-12-01 09:58:46 -0800

    functions file and plots

---
+++ functions.R
@@ -0,0 +1,40 @@
+# generate data
+gdata = function(){
+  l = list()
+  l$x = rnorm(4000, mean = 4, sd = 2)
+  l$x1 = runif(4000, min = 0, max = 4)
+  l$x2 = rpois(4000, lambda = 3)
+  l$x3 = rbinom(4000, size=3, prob = .2)
+  return(l)
+}
+
+# general stats
+gstats = function(x){
+  l = list()
+  l$s = lapply(x, summary)
+  l$r = lapply(x, range)
+  l$m = lapply(x, mean)
+  l$med = lapply(x, median)
+  l$stdev = lapply(x, sd)
+  l$t = lapply(x, t.test)
+  l$h = lapply(x, hist, col = "steelblue", xlab = "", main = "")
+  return(l)
+}
+
+# mean probability for each sample
+mean_prob = function(x){
+  s = 500
+  i = 0
+  means = mean(sample(x, s, replace = TRUE))
+  while (i < 99){
+    means = c(means, mean(sample(x, s, replace = TRUE)))
+    i = i + 1
+  }
+  m = summary(as.factor(round(means, 1)))
+  m = m / sum(m)
+  barplot(m, ylim = c(0, max(m) + 0.15),
+          main = "",
+          xlab = "Mean",
+          ylab = "Probability",
+          col = "steelblue")
+}
--- general_stats_one_mean.R
+++ general_stats_one_mean.R
@@ -1,61 +1,19 @@
 
-
 dev.off() # reset options
+par(mfrow=c(2,2))
 
 set.seed(100) 
 
-gdata = function(){
-  l = list()
-  l$x = rnorm(4000, mean = 4, sd = 2)
-  l$x1 = runif(4000, min = 0, max = 4)
-  l$x2 = rpois(4000, lambda = 3)
-  l$x3 = rbinom(4000, size=3, prob = .2)
-  return(l)
-}
+source("functions.R")
 
+# create four populations with different distributions
 d = gdata()
-
-# general stats
-gstats = function(x){
-  l = list()
-  l$s = lapply(x, summary)
-  l$r = lapply(x, range)
-  l$m = lapply(x, mean)
-  l$med = lapply(x, median)
-  l$stdev = lapply(x, sd)
-  l$t = lapply(x, t.test)
-  l$h = lapply(x, hist)
-  return(l)
-}
 
 # create a list of stats for each dataset
 dstats = gstats(d)
 
 # plot each dataset
-lapply(d, plot)
+lapply(d, plot, pch=20, col="steelblue", ylab="Value")
 
-# mean prob for each sample 
-ssize = 500
-i = 0
-q = sample(d$x, ssize)
-mean_list = mean(d$x)
-
-while (i < 100){
-  q = cbind(q, sample(d$x,ssize))
-  mean_list = c(mean_list, mean(sample(d$x,ssize,replace=TRUE)))
-  i = i + 1
-}
-
-m = summary(as.factor(round(mean_list, 1)))
-m = m / sum(m)
-barplot(m, ylim = c(0, max(m)+.15), 
-        main="title",
-        xlab="mean")
-
-
-
-# Resources 
-# https://www.investopedia.com/terms/c/central_limit_theorem.asp
-# https://en.wikipedia.org/wiki/Central_limit_theorem
-# https://www.britannica.com/science/central-limit-theorem
-# https://byjus.com/jee/central-limit-theorem/
+# plot the probability mean for each 
+lapply(d, mean_prob)
